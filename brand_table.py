ba6fd3267a74c18fb40aa13cffb017253aa6942d
Fedor Areshnikov <fareshn@mail.ru>
2024-02-20 18:55:24 +0300

    Создание структуры модуля таблицы брендов

---
+++ brand_table.py
@@ -0,0 +1,27 @@
+import pandas  # Библиотеки
+
+import json  # Debug tools
+
+
+class BrandTable:
+    def __init__(self, config: dict):
+        pass
+
+    def build(self):
+        pass
+
+    def getBrandFromPID(self, pid):  # pid – product ID
+        pass
+
+    def load(self):
+        pass
+
+    def save(self):
+        pass
+
+    def update(self, pid, brandName):  # pid – product ID
+        pass
+
+
+if __name__ == "__main__":
+    print("Debug mode")
