b5f45ddb93ba582feda0ce039c228c43c9055463
Fedor Areshnikov <fareshn@mail.ru>
2024-02-20 19:55:23 +0300

    Создан функционал инициализации таблицы брендов

--- brand_table.py
+++ brand_table.py
@@ -1,23 +1,72 @@
-import pandas  # Библиотеки
+from os import path  # Библиотеки
+import pandas
 
 import json  # Debug tools
+from time import time_ns
 
 
 class BrandTable:
     def __init__(self, config: dict):
-        pass
+        self.conf = config
+        self.table = pandas.Series(-1)
+
+        print("Инициализация таблицы брендов")
+
+        if path.isfile(self.conf['tablePath']):
+            self.load()
+        else:
+            self.build()
+
+        print("Инициализация таблицы завершена")
 
     def build(self):
-        pass
+        print("Построение таблицы брендов")
+
+        try:
+            attr = pandas.read_csv(self.conf['dataset'], encoding="ISO-8859-1")
+        except Exception:
+            print("Ошибка получения доступа к файлу датасета")
+            return
+
+        pid = []
+        brand = []
+        i = 0
+        for row in attr.iloc:
+            if row['name'] == "MFG Brand Name":
+                pid.append(int(row["product_uid"]))
+                brand.append(row["value"])
+            i += 1
+            print(i, "строк обработано")
+        self.table = pandas.Series(brand, index=pid)
+
+        print("Построение таблицы завершено")
+        self.save()
 
     def getBrandFromPID(self, pid):  # pid – product ID
         pass
 
     def load(self):
-        pass
+        print("Загрузка таблицы брендов")
+
+        try:
+            save = pandas.read_csv(self.conf['tablePath'])
+            self.table = pandas.Series(save.iloc[:, 1])
+            self.table.index = save.iloc[:, 0]
+
+            print("Загрузка таблицы завершена")
+        except Exception:
+            x = input("Ошибка загрузки таблицы брендов. Перестроить таблицу? (Y/N)\t").lower()
+            if x == 'y':
+                self.build()
 
     def save(self):
-        pass
+        print("Сохранение таблицы брендов")
+        try:
+            self.table.to_csv(self.conf['tablePath'])
+
+            print("Сохранение завершено")
+        except Exception:
+            print("Ошибка сохранения таблицы брендов")
 
     def update(self, pid, brandName):  # pid – product ID
         pass
@@ -25,3 +74,6 @@ class BrandTable:
 
 if __name__ == "__main__":
     print("Debug mode")
+    file = open("./configs/brandTableConfig.json")
+    test = BrandTable(json.load(file))
+    file.close()
