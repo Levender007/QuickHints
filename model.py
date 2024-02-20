1a0ed42daa54bfc3c6a0db3b76f2d4f7898cc349
Fedor Areshnikov <fareshn@mail.ru>
2024-02-20 21:16:39 +0300

    Создание структуры и инициализации основной части

---
+++ model.py
@@ -0,0 +1,41 @@
+from flask import current_app, redirect, render_template, url_for
+
+from time import time_ns  # Debug tools
+
+
+def main_menu(method, form):
+    if method == 'GET':
+        return render_template('main_menu.html')
+    else:
+        if form['code'] == "0":
+            current_app.config['trie'].build()
+        elif form['code'] == "1":
+            current_app.config['trie'].save()
+        elif form['code'] == "2":
+            current_app.config['brandTable'].build()
+        else:
+            current_app.config['brandTable'].save()
+        return redirect(url_for('menu'))
+
+
+def hint_model(method, form):
+    if method == 'GET':
+        return 'hint_in.html', []
+    else:
+        pass
+
+
+def trie_update(method, form):
+    if method == 'GET':
+        return render_template('trie_in.html')
+    else:
+        current_app.config['trie'].update(form['search'], {"relevance": form['relevance'], 'product_uid': form['pid']})
+        return redirect(url_for('menu'))
+
+
+def brand_update(method, form):
+    if method == 'GET':
+        return render_template('brand_in.html')
+    else:
+        current_app.config['trie'].update(form['pid'], form['brand'])
+        return redirect(url_for('menu'))
